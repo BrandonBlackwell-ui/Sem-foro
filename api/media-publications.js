7b7244b95b2461d844ed1573e936984747c5ed7c
Brandon Blackwell <brandon@blackwellstrategy.com>
2026-07-10 10:09:50 -0600

    fix: remove node: prefix from imports in media-publications.js

--- api/media-publications.js
+++ api/media-publications.js
@@ -1,5 +1,5 @@
-import fs from 'node:fs'
-import path from 'node:path'
+import fs from 'fs'
+import path from 'path'
 
 const SHEET_ID = process.env.MEDIA_SHEET_ID || '1PAcofO80aMuTNdclclqCrKS-uij0S8iI'
 const GENERAL_GID = process.env.MEDIA_SHEET_GENERAL_GID || '905402375'
